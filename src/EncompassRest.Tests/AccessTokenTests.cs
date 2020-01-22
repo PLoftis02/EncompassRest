856fc003525aa1f43d9a7c6475c7ff291cd735c2
PLoftis02 <ploftis@prmg.net>
2020-01-22 14:29:21 -0800

    Update AccessTokenTests.cs
    Test!

--- src/EncompassRest.Tests/AccessTokenTests.cs
+++ src/EncompassRest.Tests/AccessTokenTests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EncompassRest.Tests
 {
-    [TestClass]
+    [Test]
     public class AccessTokenTests : TestBaseClass
     {
         [TestMethod]
